f818f16ddfec7968378552b94a73772e3ec8ec49
LSHAVLADZE <LSHAVLADZE@WS002HP290G9>
2023-08-22 16:31:41 +0400

    Add project files.

---
+++ UniversityApi/Professor.cs
@@ -0,0 +1,13 @@
+﻿namespace UniversityApi
+{
+    public class Professor
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+       // public virtual IList<Student> { get; set; }
+    }
+}
